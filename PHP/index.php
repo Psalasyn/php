96d8b193664cdf7ca3537465a0e0f09177be9a51
Ademir José Peixer <frapeixer@gmail.com>
2025-03-08 21:48:06 -0300

    Agora vai

--- PHP/index.php
+++ PHP/index.php
@@ -1,11 +1,6 @@
 <?pho
 
-<<<<<<< HEAD
 	echo "Olá Mundo 456!";
-	echo "Olá Udemy!";
-=======
-	echo "Olá Mundo 123!";
-	echo "Olá Hcode!";
->>>>>>> 4e96bff21ea39c4c367ae2f660aa30b791e12000
+	echo "Olá Udemy e Olá Hcode!";
 
 ?>
